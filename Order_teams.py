2e6cfc286f3cd0103a38717154997111b2f71a76
Harikrishna <hkchebrolu52@gmail.com>
2021-03-30 21:58:21 +0530

    Add files via upload
    Solution for 3rd Question

---
+++ Order_teams.py
@@ -0,0 +1,26 @@
+#take input for number of test cases
+tcase=int(input())
+
+while tcase>0:
+    scores=[]
+    for test in range(3):
+        # Take points as input and covert them to integers and split them
+        points=list(map(int,input().split()))
+        #Append this points to socres list
+        scores.append(points)
+    scores=sorted(scores)
+    answer='yes'
+    count_01=0
+    count_12=0
+    for j in range(3):
+        if scores[0][j]<scores[1][j]:
+            count_01=1
+        if scores[1][j]<scores[2][j]:
+            count_12=1
+        if (scores[0][j]>scores[1][j] or scores[1][j]>scores[2][j]):
+            answer='no'
+            break
+        elif (count_01==0 or count_12==0) and j==2:
+            answer='no'
+    print(answer)
+    tcase-=1
